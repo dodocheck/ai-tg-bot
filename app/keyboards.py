761f35aa0341aa81458a175f6aa7b5cd327e9bb0
dodocheck <dodocheck@proton.me>
2025-04-03 23:26:25 +0300

    added choose_ai_model functionality

---
+++ app/keyboards.py
@@ -0,0 +1,10 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+inline_kb_aimodel = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='DeepSeek V3-0324',
+                          callback_data='deepseek/deepseek-chat-v3-0324:free')],
+    [InlineKeyboardButton(text='Gemini 2.5 Pro',
+                          callback_data='google/gemini-2.5-pro-exp-03-25:free')],
+    [InlineKeyboardButton(text='Qwen 2.5 Coder',
+                          callback_data='qwen/qwen-2.5-coder-32b-instruct:free')]
+])
--- app/user.py
+++ app/user.py
@@ -1,62 +1,80 @@
 from aiogram import Router, F
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command, CommandObject
 from loguru import logger
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from app.generator import ask_ai
-
+import app.keyboards as kb
 
 user = Router()
 
 max_tokens = 3000
 
-ai_model = 'deepseek/deepseek-chat-v3-0324:free'
-
 
 @dataclass
 class UserData:
-    context: list[dict]
-    is_busy: bool
+    """Default User obj"""
+    ai_model: str = 'deepseek/deepseek-chat-v3-0324:free'
+    context: list[dict] = field(default_factory=lambda: [
+        {'role': 'system',
+         'content': 'answer in the same language I asked you. Desired response output format: If the user query is asking for a knowledge-based answer or is specifying a data processing or coding task, immediately proceed to the direct answer written as though it was a document. There will be no additional chatty dialog from the AI unless the user is directly conversing with the AI with a chat style. AI should appear to be a data processor, not a chat partner. Keep in mind that you answers will be delivered to me as telegram messages. Answer in detail but try to avoid huge answers.'}
+    ])
+    is_busy: bool = False
 
 
 users = {}  # dict of users -> {user_id: UserData}
 
 
+@user.message(Command('model'))
+async def cmd_change_ai_model(message: Message) -> None:
+
+    user_id = message.from_user.id
+    if user_id not in users:
+        users[user_id] = UserData()
+
+    await message.answer(text='Выберите модель из списка',
+                         reply_markup=kb.inline_kb_aimodel)
+
+
+@user.callback_query(F.data)
+async def callback_change_ai_model(callback: CallbackQuery) -> None:
+
+    user_id = callback.from_user.id
+    if user_id not in users:
+        users[user_id] = UserData()
+
+    users[user_id].ai_model = callback.data
+    await callback.answer(f'Вы выбрали модель {callback.data}')
+
+
 @user.message(Command('clear'))
 async def cmd_clear(message: Message) -> None:
-    """Clears chat context for particular user
-
-    Args:
-        message (Message): message_obj from particular user
-    """
+    """Clears chat context for particular user"""
     user_id = message.from_user.id
 
     if user_id not in users:
-        users[user_id] = UserData(context=[],
-                                  is_busy=False)
+        users[user_id] = UserData()
     else:
-        users[user_id].context = []
-
-    users[user_id].context.append({'role': 'system',
-                                   'content': 'answer in the same language I asked you. Desired response output format: If the user query is asking for a knowledge-based answer or is specifying a data processing or coding task, immediately proceed to the direct answer written as though it was a document. There will be no additional chatty dialog from the AI unless the user is directly conversing with the AI with a chat style. AI should appear to be a data processor, not a chat partner. Keep in mind that you answers will be delivered to me as telegram messages. Answer in detail but try to avoid huge answers.'})
+        users[user_id].context = [{'role': 'system',
+                                   'content': 'answer in the same language I asked you. Desired response output format: If the user query is asking for a knowledge-based answer or is specifying a data processing or coding task, immediately proceed to the direct answer written as though it was a document. There will be no additional chatty dialog from the AI unless the user is directly conversing with the AI with a chat style. AI should appear to be a data processor, not a chat partner. Keep in mind that you answers will be delivered to me as telegram messages. Answer in detail but try to avoid huge answers.'},]
 
     await message.react([{"type": "emoji", "emoji": "👌"}])
 
 
 @user.message(Command('ai'))
-# If in chat - add /ai before question
 async def ask_ai_in_private(message: Message, command: CommandObject) -> None:
-    await get_ai_response(message, command.args)
+    """ If in chat - add /ai before question """
+    await _process_msg_to_ai(message, command.args)
 
 
 @user.message(F.text, F.chat.type == "private")
-# If in direct - write as it is
 async def ask_ai_in_private(message: Message) -> None:
-    await get_ai_response(message, message.text)
+    """ If in direct - write as it is """
+    await _process_msg_to_ai(message, message.text)
 
 
-async def get_ai_response(message: Message, msg_from_user: str) -> None:
+async def _process_msg_to_ai(message: Message, msg_from_user: str) -> None:
     """Writes a response to user's particular message
 
     Args:
@@ -71,10 +89,7 @@ async def get_ai_response(message: Message, msg_from_user: str) -> None:
         return
 
     if user_id not in users:
-        users[user_id] = UserData(context=[],
-                                  is_busy=False)
-        users[user_id].context.append({'role': 'system',
-                                       'content': 'answer in the same language I asked you. Desired response output format: If the user query is asking for a knowledge-based answer or is specifying a data processing or coding task, immediately proceed to the direct answer written as though it was a document. There will be no additional chatty dialog from the AI unless the user is directly conversing with the AI with a chat style. AI should appear to be a data processor, not a chat partner. Keep in mind that you answers will be delivered to me as telegram messages. Answer in detail but try to avoid huge answers.'})
+        users[user_id] = UserData()
 
     if users[user_id].is_busy:
         await message.react([{"type": "emoji", "emoji": "👨‍💻"}])
@@ -88,13 +103,13 @@ async def get_ai_response(message: Message, msg_from_user: str) -> None:
 
         await message.react([{"type": "emoji", "emoji": "✍️"}])
 
-        response, tokens_used = await ask_ai(users[user_id].context, ai_model)
+        response, tokens_used = await ask_ai(users[user_id].context, users[user_id].ai_model)
 
         if tokens_used > max_tokens:  # compressing context to 1 message to control tokens spending
             msg = 'Сделай короткий пересказ нашего диалога от третьего лица. Себя называй как Бот, а меня - Пользователь'
             users[user_id].context.append({'role': 'user',
                                            'content': msg})
-            summary, tokens_used = await ask_ai(users[user_id].context, ai_model)
+            summary, tokens_used = await ask_ai(users[user_id].context, users[user_id].ai_model)
             users[user_id].context = []
             users[user_id].context.append({'role': 'system',
                                            'content': summary})
